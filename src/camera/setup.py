7a3392f0dd9ec41991683922bed9a9ebb22cd522
ShengBin-101 <shengbin.chan@gmail.com>
2023-11-09 10:07:32 +0800

    Add Launch

--- src/camera/setup.py
+++ src/camera/setup.py
@@ -13,8 +13,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # Include all launch files.
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py'))
+        # Include all launch files. (includes both python and xml launch files)
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.xml'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
